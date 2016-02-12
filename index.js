a5c013cbddfafd77ea83b1df1a31de9dcb85dba7
Foo Bar <foobarpie.gmail.com>
2016-02-11 20:35:31 -0500

    it draws things

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
-
 #! /usr/bin/env node
 
-const args = process.argv.slice(2, process.argv.length);
+var Canvas = require('canvas')
+  , Image = Canvas.Image
+  , canvas = new Canvas(200, 200)
+  , ctx = canvas.getContext('2d');
 
-console.log(`Hello, ${args}!`);
+ctx.font = '30px Impact';
+ctx.rotate(.1);
+ctx.fillText("Awesome!", 50, 100);
 
+var te = ctx.measureText('Awesome!');
+ctx.strokeStyle = 'rgba(0,0,0,0.5)';
+ctx.beginPath();
+ctx.lineTo(50, 102);
+ctx.lineTo(50 + te.width, 102);
+ctx.stroke();
+
+console.log('<img src="' + canvas.toDataURL() + '" />');
